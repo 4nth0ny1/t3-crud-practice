2efa1a7f95fd5ee6237f2d6991594953e758a773
Anthony Catullo <anthonycatullo@oatslam.local>
2023-06-04 20:07:23 -0500

    restored AuthShowcase

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -43,6 +43,7 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <h1 className="text-3xl text-white">t3 crud practice</h1>
+        <AuthShowcase />
         <Posts />
       </main>
     </>
